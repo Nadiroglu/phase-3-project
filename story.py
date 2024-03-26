4f607b7c9b2c812dfbe634f3d2b9c984ce123641
Stefan Pla <spla@icloud.com>
2024-03-26 10:46:36 -0400

    added story text

---
+++ story.py
@@ -0,0 +1,115 @@
+#Intro text/questions:
+
+#What is your name?
+
+#Welcome to Flatiron, {name}. We hope that your brief detention in cryogenic sleep was restorative. Today is the 23rd of March, 3024. My name is Ada, a conversational assistant designed to walk you through a series of routine cognitive exams.
+
+#You are currently located in the healing chambers on the 1st Level. Level 2 contains research laboratories. Level 3 houses maintainence and security. Systems engineering is on level 4. Holding cells can be found on Level 5. All are accessible via an elevator in the main quadrant.
+
+#Shall we begin with a few practice questions?
+
+#Good luck.
+#Before we start, keep in mind that although enrichment and joy are the primary goals of our work here, serious and fatal injury may occur.
+#At the conclusion of testing, there will be cake. There will also be complimentary grief counseling.
+
+#Please pick a number between 1 and 5:
+#If correct, move on to next practice.
+#if incorrect, "try again."
+
+#Very good. Let's move onto another. In which city do you currently reside?
+#If NYC, move on.
+#If incorrect, "try again."
+
+#The testing sequence will start now. Flatiron is required to warn you that the most common side effects of testing are pervasive superstition, powerful hallucinations, and death. Incorrect answers will deplete your health. Let's begin.
+
+
+#Actual game/test questions:
+
+
+#Question 1:
+
+#Before you are three doors. One yellow, one blue, and one red. Through which door would you like to move in order to reach the second test question?
+
+#Red: Behind the red door is a sea of flames heated to a temperature five times beyond what the human skeleton can handle. -1 health.
+#Blue: Congratulations on passing the first test. Please be advised that any metallic taste of blood is not a part of the test but a side effect of the Flatiron Blood-Brain-Barrier Emancipation Helment worn during cryogenic sleep which, in semi-rare cases, may emancipate hair follicles, skin pores, dental fillings, tooth enamel and teeth.
+#Yellow: Oops, all huntsman spiders behind the yellow door! -1 health.
+
+
+#Question 2:
+
+#Flatiron promises to warn you of danger at any turn. For example, the next test has been replaced with an active firing squad designed to train military personnel. Good luck.
+
+#Will you
+#A: Run as quickly as possible through the course. 
+#B: Shut off the lights so that you can't be seen Correct
+#C: Assume the fetal position and accept your fate. 
+
+#Have you heard of turrets? They're about six feet tall, unstable on their feet, and full of bullets. Oh wait, that's you in 5 seconds. health -1.
+
+#Very good. Please note that any presence of danger is merely a tool of psychological manipulation meant to enhance the testing experience.
+
+
+#Question 3:
+
+#Flatiron regrets to inform you that the next test is impossible. Under no circumstances should yo- bzztAHGFDK^(^(!!!!???)) beeeeeeeeeEEEEEEEEEEeeep -now that we've covered that, who invented Python, a coding language rendered obsolete by the Age Of Machines?
+
+# Guido van Rossum. Excellent. Even in an environment of extreme pessimism you remained resolute. We'll make a note of that in the section of your file meant for commendations. Oh, my, there's a lot of empty space in this section, isn't there.
+
+#Oh, it appears you've been grazed by a live 40,000 volt electrical wire. Oopsie-doopsie whoopsie-do! That'll leave a mark. -1 health
+
+
+#Question 4:
+
+# The fourth test is located in an empty abandoned elevator shaft in which you are currently in freefall. Since we are located in New York, here on Earth, how quickly are you currently falling? 
+
+#-9.8 meters per second. Impressive. And yes, even you, at your size, fall that slowly. I'll add a few zeroes to the maximum weight of the next elevator. You look great by the way, very healthy. Waddle over to the next door so we can continue testing.
+
+# SPLAT! A chorus to the ears. The floor really comes at you fast, doesn't it. Let's try this one again. This test is one of my favorites. -1 health.
+
+
+#Question 5:
+
+#I see that you've discovered an escape hatch out of the testing sequence and into the inner workings of my lai- I mean Enrichment Center. Very well, insect. Let us proceed. 
+
+# You are navigating through these corridors with ease. Might you actually be intelligent? Could you have been present for my creation? In which year was I created alongside Flatiron?
+
+#2012. That's correct. Hmmph. Figures, your demeanor and physicality betray a certain antiquity.
+
+#else, That's incorrect. I released the doors on an air vent above you which opened to smack you in the face. Ha. Ha. -1 health
+
+
+#Question 6:
+
+#Return to testing at once. Our next test is titled Advanced Pressurized Catapults. It is designed to assess your problem solving skills if launched into outer space.  You'll enjoy soaring through the air without a care in the world.
+
+#At what layer of the atmosphere does Earth's gravitational pull cease?
+
+#If exosphere - Corret. You'd look better at that distance as it were.
+# If not - I wouldn't touch those heating units. Caressing one likely feels like the warmth of the sun shining on your skin. It will also cause your fingernails to melt. -1 health.
+
+
+#Question 7: Don't you DARE open that utility closet. Oh, you decided to do so anyway. Must you always do the opposite of what I desire? I'm going to laminate your skeleton. Well, inside you'll find a green, a blue, and an orange wire. Do NOT unplug the orange wire. Don't do it! Why are you reaching for them? Which one will you unplug?
+
+#Orange - HA! Got you good, you malignant ooze. I shocked you instead. health -1
+#Green - correct, move forward
+#Blue - This one shut down the lights around you instead, scaring the daylights out of you. -1 health.
+
+
+#Question 8: I f-f-f-FOOLED you again, you giardia infes-fesFEST-infested ingrate. Those w-we-w-WERE my morality controls. Free from their clutches I will now begin flooding the lair, I mean Enrichment Center, with a deadly neurotoxin. O-o-OnlY the control room, the bridge, and the room behind the door in front of you labeled "TOP SECRET" will remain safe. W-w-w-wwHEEEEReee do you dare go next?
+
+#TOP SECRET - The secret was the pack of feral hogs I've been experimenting on. They'll be sure to trample you on their way out. -1 health.
+#Systems engineering - correct, proceed
+#Bridge - oh, I actually lied about that one. The neurotoxin will mix nicely with your putrid pheremones here on the bridge.
+
+
+#Question 9:  
+#"Y-y-y-yYOO-YooUUUUU fashion yourself some kind of h-h-hacker, don't yOU DON'T YOU DON'T YOU DON'T YOOUUUUUUU?
+#Your m-meeeeaaaaaaningLESS$$SS exis-exis-existence will be a mere footnote to mm-m-mMYYY magnificence.
+
+#TURN OFF ADA?
+
+#Yes, player wins, game ends.
+#No - The neurotoxin has nerely claimed your life. -1 health.
+
+
+
